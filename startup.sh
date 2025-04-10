924b947b65a6f33b02e6741079763fcd16370080
paulcristian.mocanu <paulcristian.mocanu@bytedance.com>
2025-04-10 15:50:39 +0100

    removed the github token
    Change-Id: I02207b65844bb004b2304bcbaa47580b7f62050f

--- startup.sh
+++ startup.sh
@@ -7,9 +7,6 @@ python3.11 -m pip install --upgrade pip
 python3 -m pip install "tensorflow[and-cuda]" pandas optuna kagglehub
 git config --global user.name "Paul"
 git config --global user.email "mocanupaulc@gmail.com"
-
-token=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
-
 pip install \
   --extra-index-url https://pypi.nvidia.com \
   tensorrt-bindings==8.6.1 \
